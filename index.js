a71d0bdc6039f95ff9eb58a100d2fa07d20c45a8
Olga Nadongo <winnienadongo@gmail.com>
2022-06-04 23:42:42 +0300

    commit tree 5d66589b3e8593c3d9fee1a53c0c33e3150193fd

---
+++ index.js
@@ -0,0 +1,45 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+
+function add(){
+  
+}
+function subtract(){
+
+}
+function multiply(){
+  
+}
+function divide(){
+  
+}
+
+function add(a , b){
+  return a+b
+}
+function subtract(a, b){
+  return a-b  
+}
+function multiply(a , b){
+  return a*b
+}
+function divide(a , b){
+  return a/b
+}
+function increment(a){
+  return a+=1
+}
+function decrement(a){
+  return a-=1
+}
+
+
+function makeInt(n){
+
+ return parseInt(n, 10)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
